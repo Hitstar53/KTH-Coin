c966b6a08da9006e2b4a4ec9f6652751b3c6ec0f
Hatim Sawai <hatimsawai76@gmail.com>
2022-11-02 20:40:03 +0530

    Updates
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- src/Blockchain.java
+++ src/Blockchain.java
@@ -3,30 +3,104 @@ public class Blockchain {
     class Block {
         String hash;
         String prevHash;
-        int data;
+        Block next;
+        Block prev;
+        //int data;
+        Transaction transaction;
         long timeStamp;
+        // Constructor
+        Block() {
+            next = null;
+            prev = null;
+        }
     }
     Block genesisBlock;
+    Block omegaBlock;
     //constructor
     public Blockchain() {
         genesisBlock = new Block();
-        genesisBlock.data = 0;
         genesisBlock.timeStamp = System.currentTimeMillis();
         genesisBlock.prevHash = "";
         genesisBlock.hash = calculateHash(genesisBlock);
+        omegaBlock = genesisBlock;
     }
-    //insert mthod for blockchain
-    public void addBlock(int data) {
+    //add a new block to the blockchain
+    public void addBlock(Transaction transaction) {
         Block newBlock = new Block();
-        newBlock.data = data;
+        //newBlock.data = data;
+        //assign transaction details to new block
+        newBlock.transaction = new Transaction();
+        newBlock.transaction.sender = transaction.sender;
+        newBlock.transaction.receiver = transaction.receiver;
+        newBlock.transaction.amount = transaction.amount;
         newBlock.timeStamp = System.currentTimeMillis();
-        newBlock.prevHash = genesisBlock.hash;
+        newBlock.prevHash = omegaBlock.hash;
         newBlock.hash = calculateHash(newBlock);
-        genesisBlock = newBlock;
+        omegaBlock.next = newBlock;
+        newBlock.prev = omegaBlock;
+        omegaBlock = newBlock;
+    }
+    //traversal the blockchain
+    public void traverse() {
+        Block currentBlock = genesisBlock;
+        while (currentBlock != null) {
+            if (currentBlock.transaction != null) {
+                System.out.println(currentBlock.transaction.toString());
+            }
+            System.out.println("Hash: " + currentBlock.hash);
+            System.out.println("PrevHash: " + currentBlock.prevHash);
+            System.out.println("Timestamp: " + currentBlock.timeStamp);
+            System.out.println();
+            currentBlock = currentBlock.next;
+        }
+    }
+    //delete a block from the blockchain
+    /*public void deleteBlock(int data) {
+        Block currentBlock = genesisBlock;
+        while (currentBlock != null) {
+            if (currentBlock.data == data) {
+                if (currentBlock == genesisBlock) {
+                    genesisBlock = genesisBlock.next;
+                    genesisBlock.prev = null;
+                } else if (currentBlock == omegaBlock) {
+                    omegaBlock = omegaBlock.prev;
+                    omegaBlock.next = null;
+                } else {
+                    currentBlock.prev.next = currentBlock.next;
+                    //exchange hashes
+                    currentBlock.next.prevHash = currentBlock.prevHash;
+                    currentBlock.next.prev = currentBlock.prev;
+                }
+            }
+            currentBlock = currentBlock.next;
+        }
+    }*/
+    //delete block using trasaction data
+    public void deleteBlock(Transaction transaction) {
+        Block currentBlock = genesisBlock;
+        while (currentBlock != null) {
+            if (currentBlock.transaction == transaction) {
+                if (currentBlock == genesisBlock) {
+                    genesisBlock = genesisBlock.next;
+                    genesisBlock.prev = null;
+                } else if (currentBlock == omegaBlock) {
+                    omegaBlock = omegaBlock.prev;
+                    omegaBlock.next = null;
+                } else {
+                    currentBlock.prev.next = currentBlock.next;
+                    //exchange hashes
+                    currentBlock.next.prevHash = currentBlock.prevHash;
+                    currentBlock.next.prev = currentBlock.prev;
+                }
+            }
+            currentBlock = currentBlock.next;
+        }
     }
     //calculate hash method
     public String calculateHash(Block block) {
-        String dataToHash = block.prevHash + Long.toString(block.timeStamp) + Integer.toString(block.data);
+        //ternary operator
+        String data = (block.transaction == null) ? "" : block.transaction.toString();
+        String dataToHash = block.prevHash + Long.toString(block.timeStamp)+data;
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] bytes = digest.digest(dataToHash.getBytes("UTF-8"));
--- src/Transaction.java
+++ src/Transaction.java
@@ -2,10 +2,11 @@ public class Transaction {
     public String sender;
     public String receiver;
     public int amount;
-    public Transaction(String sender, String receiver, int amount) {
-        this.sender = sender;
-        this.receiver = receiver;
-        this.amount = amount;
+    //constructor
+    public Transaction() {
+        sender = "";
+        receiver = "";
+        amount = 0;
     }
     //buy method
     public void buy(String receiver, int amount) {
@@ -16,5 +17,9 @@ public class Transaction {
     public void sell(String sender, int amount) {
         this.sender = sender;
         this.amount = amount;
+    }
+    //toString method
+    public String toString() {
+        return "Sender: " + sender + ", Receiver: " + receiver + ", Amount: " + amount;
     }
 }
